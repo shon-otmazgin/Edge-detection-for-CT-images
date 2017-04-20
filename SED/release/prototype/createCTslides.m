6b616624961b69ff4ba4c51a761488017020f5e5
Shon Otmazgin <shon711@gmail.com>
2017-04-20 22:56:32 +0300

    optimazing createCTslides function

--- SED/release/prototype/createCTslides.m
+++ SED/release/prototype/createCTslides.m
@@ -1,15 +1,21 @@
-function [ ] = createCTslides( scan_name, folder)
-%UNTITLED Summary of this function goes here
+function [ ] = createCTslides(volume,scan_name, folder)
 %   the function is getting CT scan name and serach it on 'Volumes' folder.
 %   once the scan it found, the function create slides from the scan and
-%   save the slides on the curent folder under 'edges' folder.
-    CTscan = load_untouch_nii_gzip(scan_name);
-
-    scanMat = CTscan.img;
+%   save the slides on the curent folder under 'slides' folder.
+    
+    scanMat = volume;
     z = size(scanMat,3);
+    
     workingDir = folder;
+    
+    itemFolderName = strsplit(scan_name,'.');
+    workingDir = fullfile(workingDir,itemFolderName(1));
+    workingDir = strjoin(workingDir);
+    workingDir = fullfile(workingDir,'Volume');
+    mkdir(workingDir);   %create the directory
+   
     for i=1:z
-        filename = [sprintf('%03d',i) '.jpg'];
+        filename = [sprintf('%03d',i) '.png'];
         fullname = fullfile(workingDir, filename);
         imwrite(scanMat(:,:,i) ,fullname);
     end
